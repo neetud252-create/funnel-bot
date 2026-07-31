38d3ccd0e4483c8c87414ca68f47667c53f397c6
topx <neetud252@gmail.com>
2026-07-31 11:03:24 +0530

    Bot uses database

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import asyncio, os, logging
+import db
 from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart
 from aiogram.types import Message
@@ -8,9 +9,11 @@ dp = Dispatcher()
 
 @dp.message(CommandStart())
 async def start(m: Message):
+    await db.touch_user(m.from_user.id, m.from_user.username)
     await m.answer("alive")
 
 async def main():
+    await db.connect()
     bot = Bot(os.environ["BOT_TOKEN"])
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
